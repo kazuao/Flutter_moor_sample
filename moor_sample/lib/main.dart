d1a169063e8c90edbf3f1d968d9951cadcf2dfd7
[STV]Kazunori Aoki <k.aoki@stv-tech.co.jp>
2021-03-21 16:05:28 +0900

    最初から

--- moor_sample/lib/main.dart
+++ moor_sample/lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:moor_sample/data/moor_database.dart';
-import 'package:provider/provider.dart';
 
-import 'ui/home_page.dart';
-import 'data/moor_database.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,12 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Provider(
-      builder: (_) => AppDatabase(),
-      child: MaterialApp(
-        title: 'Material App',
-        home: HomePage(),
-      ),
-    );
+    return();
   }
 }
